a4e0c9518fe6d47c94a1eb63d79f85b6b1b80dbb
jianghao <jiang.hao@ikcrm.com>
2017-07-24 18:00:26 +0800

    react jsx

---
+++ src/helloworld_react.js
@@ -0,0 +1,14 @@
+// var myStyle = {
+//     color: "FF0000",
+//     fontSize: '100px'
+// };
+// var arr = [
+//     <h1>hello, jason</h1>,
+//     <h2>梦想任你飞！</h2>
+// ]
+// ReactDOM.render(
+//     <div>{arr}</div>,
+//     document.getElementById("example")
+// );
+var mydivElement = <div className="foo">jason</div>;
+ReactDOM.render(mydivElement, document.getElementById('example'));
