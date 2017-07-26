eb5a77be7558152949f0b7a7307da1eb2b40074e
jianghao <jiang.hao@ikcrm.com>
2017-07-26 11:32:54 +0800

    add

---
+++ src/react_props_child.js
@@ -0,0 +1,21 @@
+var NodeList = React.createClass({
+    render: function () {
+        return (
+            <ol>
+                {
+                    React.Children.map(this.props.children, function (child) {
+                       return <li>{child}</li>
+                    })
+                }
+            </ol>
+        );
+    }
+});
+
+ReactDOM.render(
+    <NodeList>
+        <span>jason</span>
+        <span>Bob</span>
+    </NodeList>,
+    document.getElementById('message')
+);
