dcdad2bf9d604b9076b0510e36688f564305746d
jianghao <jiang.hao@ikcrm.com>
2017-11-09 17:57:48 +0800

    TodoList

---
+++ src/containers/App.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { addTodo, completeTodo, setVisibilityFilter, VisibilityFilters } from "../action/actions";
+import AddTodo from '../components/AddTodo';
+import TodoList from '../components/TodoList';
+import Footer from '../components/Footer';
+
+class App extends Component {
+  render() {
+    const { dispatch, visibleTodos, VisibilityFilter } = this.props;
+    return (
+      <div>
+        <AddTodo
+          onAddClick={text =>
+          dispatch(addTodo(text))
+        }/>
+        <TodoList
+          todos={visibleTodos}
+          onTodoClick={index =>
+          dispatch(completeTodo(index))
+        }/>
+        <Footer
+          filter={VisibilityFilter}
+          onFilterChange={nextFilter =>
+          dispatch(setVisibilityFilter(nextFilter))
+        }/>
+      </div>
+    )
+  }
+}
+
+App.propTypes = {
+  visibleTodos: PropTypes.arrayOf(PropTypes.shape({
+    text: PropTypes.string.isRequired,
+    completed: PropTypes.bool.isRequired
+  }).isRequired).isRequired,
+  VisibilityFilter: PropTypes.oneOf([
+    'SHOW_ALL',
+    'SHOW_COMPLETED',
+    'SHOW_ACTIVE'
+  ]).isRequired
+};
+
+function selectTodos(todos, filter) {
+  switch (filter) {
+    case VisibilityFilters.SHOW_ALL:
+      return todos
+    case VisibilityFilters.SHOW_COMPLETED:
+      return todos.filter(todo => todo.completed)
+    case VisibilityFilters.SHOW_ACTIVE:
+      return todos.filter(todo => !todo.completed)
+  }
+}
+
+
+function select(state) {
+  return {
+    visibleTodos: selectTodos(state.todos, state.visibilityFilter),
+    VisibilityFilter: state.visibilityFilter
+  }
+}
+
+export default connect(select)(App);
--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import './App.css';
-import App from "./welcome";
 import Comment from './Comment';
 import Clock from './Clock';
 import Toggle from './Toggle'
@@ -19,49 +18,58 @@ import ListMap from './list_map';
 import Post from "./Post";
 import Card from "./Card";
 
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import App from './containers/App';
+import todoApp from './reducer/reducer';
+
+let store = createStore(todoApp);
+
 class CommentBox extends Component {
-    render() {
-        return (
-            <div className="commentBox">
-                {/*<h1>Comments</h1>*/}
-                {/*<App name="Sara"/>*/}
-                {/*<Comment date={ comment.date }*/}
-                         {/*text={ comment.text }*/}
-                         {/*author={ comment.author }>*/}
-                {/*</Comment>*/}
-                {/*<Clock/>*/}
-                {/*<Toggle/>*/}
-                {/*<LoginControl/>*/}
-                {/*<Mailbox unreadMessages={ messages }/>*/}
-                {/*<WarningPage/>*/}
-                {/*<NumberList numbers={ numbers }/>*/}
-                {/*<Blog post={ post }/>*/}
-                {/*<NameForm/>*/}
-                {/*<FlavorForm/>*/}
-                {/*<Reservation/>*/}
-                {/*<Calculator/>*/}
-                {/*<LikeButton wordings={{likedText: '已赞', unlikedText: '赞'}}/>*/}
-                {/*<br/>*/}
-                {/*<Computer/>*/}
-                {/*<ListMap/>*/}
-                {/*<Post content={123}/>*/}
-                {/*<Card>*/}
-                  {/*<h2>React.js 小书</h2>*/}
-                  {/*<p>开源、免费、专业、简单</p>*/}
-                  {/*订阅： <input/>*/}
-                {/*</Card>*/}
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="commentBox">
+        <Provider store={store}>
+          <App />
+        </Provider>
+        <h1>Comments</h1>
+        <Comment date={ comment.date }
+        text={ comment.text }
+        author={ comment.author }>
+        </Comment>
+        <Toggle/>
+        <LoginControl/>
+        <Mailbox unreadMessages={ messages }/>
+        <WarningPage/>
+        <NumberList numbers={ numbers }/>
+        <Blog post={ post }/>
+        <NameForm/>
+        <FlavorForm/>
+        <Reservation/>
+        <Calculator/>
+        <LikeButton wordings={{likedText: '已赞', unlikedText: '赞'}}/>
+        <br/>
+        <Computer/>
+        <ListMap/>
+        <Post content={123}/>
+        <Card>
+        <h2>React.js 小书</h2>
+        <p>开源、免费、专业、简单</p>
+        订阅： <input/>
+        </Card>
+        <Clock/>
+      </div>
+    );
+  }
 }
 
 const comment = {
-    date: new Date(),
-    text: 'I hope you enjoy learning React!',
-    author: {
-        name: 'hello 包子!',
-        avatarUrl: 'http://placekitten.com/g/64/64'
-    }
+  date: new Date(),
+  text: 'I hope you enjoy learning React!',
+  author: {
+    name: 'hello 包子!',
+    avatarUrl: 'http://placekitten.com/g/64/64'
+  }
 };
 
 const messages = ['React', 'facebook'];
@@ -69,8 +77,8 @@ const messages = ['React', 'facebook'];
 const numbers = [1, 2, 3, 4, 5];
 
 const post = [
-    {id: 1, title: 'Hello World', content: 'Welcome to learning React!'},
-    {id: 2, title: 'Installation', content: 'You can install React from npm.'}
+  {id: 1, title: 'Hello World', content: 'Welcome to learning React!'},
+  {id: 2, title: 'Installation', content: 'You can install React from npm.'}
 ];
 
 export default CommentBox;
